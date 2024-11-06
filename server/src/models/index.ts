b59bc87d8a78542b697af95a74e62e38dc9171a6
Buster <yahyemohamed2002@gmail.com>
2024-11-06 14:20:54 -0600

    added the models to index.ts file

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,6 +1,13 @@
 import sequelize from '../config/connection.js';
 import { UserFactory} from './user.js';
+import { GameFactory} from './game.js';
+import { LibraryEntryFactory} from './libraryEntry.js';
 
 const User = UserFactory(sequelize);
+const Game = GameFactory(sequelize);
+const LibraryEntry = LibraryEntryFactory(sequelize);
 
-export { User};
+User.hasMany(LibraryEntry, { foreignKey: 'userId', onDelete: 'CASCADE' });
+LibraryEntry.belongsTo(User, { foreignKey: 'userId' })
+
+export { User, Game, LibraryEntry};
