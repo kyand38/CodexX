8ea2e4d39843dc18b7f985d772c801a2d8662d4a
modifiedyoke <b.shurts@outlook.com>
2024-11-07 09:59:39 -0600

    added more api calls, interfaces

--- client/src/service/rawgService.ts
+++ client/src/service/rawgService.ts
@@ -1,83 +1,134 @@
-import { RawgGame } from '../interfaces/RawgGame';
+// Basic usage here:
+// For each category, like games, creators, publishers, etc. there will be three functions:
+// 
+//  getGame(id: number)                       - returns one specific game object
+//  getGames(perPage: number, page: number)   - returns an array of game objects
+//  searchGames(game: string)                 - returns an array of game objects, or one game object
+//
+// Other categories will work similarily, getCreator, getCreators, searchCreators
+
+import {
+  RawgCreator,
+  RawgGame,
+  RawgGames,
+  RawgCreators,
+  RawgPublisher,
+  RawgPublishers,
+  RawgDeveloper,
+  RawgDevelopers
+} from '../interfaces/RawgGame';
 
 import dotenv from 'dotenv';
 dotenv.config();
-
-// class GameAPI {
-//   id: number;
-//   name: string;
-//   description: string;
-//   metacritic: number;
-//   released: string;
-//   background_image: string;
-//   website: string;
-//   playtime: number;
-//   metacritic_url: string;
-
-//   constructor(
-//     id: number,
-//     name: string,
-//     description: string,
-//     metacritic: number,
-//     released: string,
-//     background_image: string,
-//     website: string,
-//     playtime: number,
-//     metacritic_url: string,
-//   ) {
-//     this.id = id;
-//     this.name = name;
-//     this.description = description;
-//     this.metacritic = metacritic;
-//     this.released = released;
-//     this.background_image = background_image;
-//     this.website = website;
-//     this.playtime = playtime;
-//     this.metacritic_url = metacritic_url;
-//   }
-// }
 
 class RawgService {
 
   private REQUEST_URL = 'https://api.rawg.io/api/';
   private API_KEY = process.env.RAWG_API_KEY;
 
-  async getGame(gameId: number) {
-
-    const response = await fetch(`${this.REQUEST_URL}/games/${gameId}?key=${this.API_KEY}`, {
+  // --------------GAMES BEGIN-----------------
+  async getGame(id: number): Promise<RawgGame> {
+    const response = await fetch(`${this.REQUEST_URL}/games/${id}?key=${this.API_KEY}`, {
       method: 'GET',
     });
-
-    const game = await response.json();
-    console.log(game);
-    return JSON.parse(game);
-
+    const data = await response.json();
+    return JSON.parse(data);
   }
 
-  async getGames() {
-
-    const response = await fetch(`${this.REQUEST_URL}/games?key=${this.API_KEY}`, {
+  async getGames(perPage: number, page: number): Promise<RawgGames> {
+    const response = await fetch(`${this.REQUEST_URL}/games?page=${page}&page_size=${perPage}&key=${this.API_KEY}`, {
       method: 'GET',
     });
-
-    const games = await response.json();
-    // console.log(games);
-    return JSON.parse(games);
-
+    const data = await response.json();
+    return JSON.parse(data);
   }
 
-  async searchGame(gameName: string) {
-
-    const response = await fetch(`${this.REQUEST_URL}/games?${gameName}&key=${this.API_KEY}`, {
+  async searchGame(gameName: string): Promise<RawgGames | RawgGame> {
+    const response = await fetch(`${this.REQUEST_URL}/games?search=${gameName}&key=${this.API_KEY}`, {
       method: 'GET',
     });
+    const data = await response.json();
+    return JSON.parse(data);
+  }
+  // ----------------GAMES END-----------------------
 
-    const results = await response.json();
-    // console.log(game);
-    return JSON.parse(results);
-
+  // ------------------CREATORS BEGIN----------------
+  async getCreators(perPage: number, page: number): Promise<RawgCreators> {
+    const response = await fetch(`${this.REQUEST_URL}/creators?page=${page}&page_size=${perPage}&key=${this.API_KEY}`, {
+      method: 'GET',
+    });
+    const data = await response.json();
+    return JSON.parse(data);
   }
 
+  async searchCreator(name: string): Promise<RawgCreator | RawgCreators> {
+    const response = await fetch(`${this.REQUEST_URL}/creators?search=${name}&key=${this.API_KEY}`, {
+      method: 'GET',
+    });
+    const data = await response.json();
+    return JSON.parse(data);
+  }
+
+  async getCreator(id: number): Promise<RawgCreator> {
+    const response = await fetch(`${this.REQUEST_URL}/creators/${id}?key=${this.API_KEY}`, {
+      method: 'GET',
+    });
+    const data = await response.json();
+    return JSON.parse(data);
+  }
+  // -------------------CREATORS END-----------------------
+
+  // -------------------PUBLISHERS BEGIN-------------------
+  async getPublishers(perPage: number, page: number): Promise<RawgPublishers> {
+    const response = await fetch(`${this.REQUEST_URL}/publishers?page=${page}&page_size=${perPage}&key=${this.API_KEY}`, {
+      method: 'GET',
+    });
+    const data = await response.json();
+    return JSON.parse(data);
+  }
+
+  async searchPublisher(name: string): Promise<RawgPublisher | RawgPublishers> {
+    const response = await fetch(`${this.REQUEST_URL}/publisher?search=${name}&key=${this.API_KEY}`, {
+      method: 'GET',
+    });
+    const data = await response.json();
+    return JSON.parse(data);
+  }
+
+  async getPublisher(id: number): Promise<RawgPublisher> {
+    const response = await fetch(`${this.REQUEST_URL}/creators/${id}?key=${this.API_KEY}`, {
+      method: 'GET',
+    });
+    const data = await response.json();
+    return JSON.parse(data);
+  }
+  // -------------------PUBLISHERS END---------------------
+
+  // --------------DEVELOPERS BEGIN---------------------------
+  async getDevelopers(perPage: number, page: number): Promise<RawgDevelopers> {
+    const response = await fetch(`${this.REQUEST_URL}/developers?page=${page}&page_size=${perPage}&key=${this.API_KEY}`, {
+      method: 'GET',
+    });
+    const data = await response.json();
+    return JSON.parse(data);
+  }
+
+  async searchDevelopers(name: string): Promise<RawgDeveloper | RawgDevelopers> {
+    const response = await fetch(`${this.REQUEST_URL}/developers?search=${name}&key=${this.API_KEY}`, {
+      method: 'GET',
+    });
+    const data = await response.json();
+    return JSON.parse(data);
+  }
+
+  async getDeveloper(id: number): Promise<RawgDeveloper> {
+    const response = await fetch(`${this.REQUEST_URL}/developers/${id}?key=${this.API_KEY}`, {
+      method: 'GET',
+    });
+    const data = await response.json();
+    return JSON.parse(data);
+  }
+  // ----------------DEVELOPERS END----------------------
 }
 
 const rawgSerivice = new RawgService();
