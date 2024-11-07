6aac76d7c7eb67f9d98b1ea1c6b88d034e52d064
Buster <yahyemohamed2002@gmail.com>
2024-11-06 21:16:05 -0600

    added seed data for user_seeds.ts file

--- server/src/seeds/game_seeds.ts
+++ server/src/seeds/game_seeds.ts
@@ -1,3 +1,7 @@
 //Store each game by integer Id
 //title, genre, platform, releaseDate, imageUrl
 import { Game } from "../models/index.js"; 
+
+export const seedGame = async () => {
+    await Game.bulkCreate
+}
--- server/src/seeds/user_seeds.ts
+++ server/src/seeds/user_seeds.ts
@@ -1,1 +1,39 @@
+//username, password, and email attributes needed
+
 import { User } from '../models/index.js'
+import bcrypt from 'bcrypt';
+
+//sample data to be seeded into database
+export const seedUser = async () => {
+    try {
+        const users = [
+
+            {
+                username: 'buster',
+                password: await bcrypt.hash('12345678', 10), // hashed password
+                email: 'email@email.com'
+            },
+            {
+                username: 'guster',
+                password: await bcrypt.hash('87654321', 10), // hashed password
+                email: 'gmail@gmail.com'
+            },
+            {
+                username: 'tuster',
+                password: await bcrypt.hash('abcdefgh', 10), // hashed password
+                email: 'yahoo@yahoo.com'
+            },
+            {
+                username: 'wester',
+                password: await bcrypt.hash('ijklmnop', 10), // hashed password
+                email: 'hotmail@hotmail.com'
+            },
+
+        ]
+        await User.bulkCreate(users)
+        console.log('Users have been successfully seeded.');
+    }
+    catch (error) {
+        console.error('Error seeding users:', error)
+    }
+};
