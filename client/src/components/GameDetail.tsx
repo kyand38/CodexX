c41d1c6ed9ab865faa983174c997020da27fcea1
Kyle Anderson <kyand2024@gmail.com>
2024-11-21 18:38:15 -0600

    Update GameDetail.tsx

--- client/src/components/GameDetail.tsx
+++ client/src/components/GameDetail.tsx
@@ -42,9 +42,9 @@ const GameDetail: React.FC<GameDetailProps> = () => {
 
   if (!game) {
     return <div id="loading-message">Loading game details...</div>; // Handle loading state
+       listDevelopmentTeam(devs);
   }
 
-  // const devTeamString = listDevelopmentTeam(devs);
 
   return (
     <div className="game-detail">
